707bfed2840ae8184ac894ae950f2e2aefb86f14
Akshaj Dev <akshajdevkv@gmail.com>
2025-06-05 06:49:36 +0530

    refactor: component naming convention

---
+++ client/src/components/Public.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+import React from 'react'
+
+const Public = () => {
+  return (
+    <div>
+      <h1>Public</h1>
+    </div>
+  )
+}
+
+export default Public
