37ba3caa4a83b53dabf59159866b972de0b54b39
Akshaj Dev <akshajdevkv@gmail.com>
2025-06-07 11:32:50 +0530

    feat: add Redux and RTK Query setup

--- client/src/features/users/UsersList.jsx
+++ client/src/features/users/UsersList.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import { useGetUsersQuery } from "./usersApiSlice"
 
 const UsersList = () => {
-  return (
-    <div>
-      <h1>Users List</h1>
-    </div>
-  )
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery()
+
+    let content
+
+    if (isLoading) content = <p>Loading...</p>
+
+    if (isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+        const { ids } = users
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+
+        content = (
+            <table className="table table--users">
+                <thead className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th user__username">Username</th>
+                        <th scope="col" className="table__th user__roles">Roles</th>
+                        <th scope="col" className="table__th user__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+
+    return (
+        <div className="users-list">
+            <h1>Users List</h1>
+            {content}
+        </div>
+    )
 }
 
-export default UsersList;
+export default UsersList
